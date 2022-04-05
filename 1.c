d98cb49ddedbc0126428eddc396c51fbbb3bfb4b
Shaun-maker <a.paillaud75@gmail.com>
2022-04-05 07:03:45 +0200

    Premiers exercice

--- 1.c
+++ 1.c
@@ -1,8 +1,24 @@
+// Ecrire un programme qui saisit deux entiers et affiche leur produit. Modifier ensuite le programme afin de saisir deux réels.
+
 #include <stdio.h>
+#include <string.h>
+
+//functions prototypes
+int get_int();
 
 int main(void)
 {
-    int a = 1;
-    int b = 2;
-    printf("Hello !\n");
+    int number = get_int();
+}
+
+//Fonction qui demande de saisir un nombre réels
+
+int get_int()
+{
+    int number;
+    scanf("%i", number);
+    for (int i = 0, n = strlen(number); i < n; i++)
+    {
+
+    }
 }
