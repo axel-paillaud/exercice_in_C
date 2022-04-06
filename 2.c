97af9d36c5969ed0e26e474ea7b1bc4ea10eea14
Shaun-maker <a.paillaud75@gmail.com>
2022-04-06 19:45:40 +0200

    Exercice 2 en C

--- 2.c
+++ 2.c
@@ -1,10 +1,22 @@
+// Ecrire un programme qui échange deux entiers saisis. Afficher les entiers avant et après l'échange
+
 #include <stdio.h>
 
 int main(void)
 {
-    int a = 1;
-    int b = 2;
-    int c = 3;
-    int d = 4;
-    int e = 5;
+    int x, y;
+
+    printf("Saisir la valeur de x: ");
+    scanf("%i", &x);
+
+    printf("Saisir la valeur de y: ");
+    scanf("%i", &y);
+
+    printf("Avant l'échange, la valeur de x est de: %i et la valeur de y est de: %i\n", x, y);
+
+    int tmp = y;
+    y = x;
+    x = tmp;
+
+    printf("Après l'échange, la valeur de x est de: %i et la valeur de y est de: %i\n", x, y);
 }
